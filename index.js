94ec89a89821d36335a8a6b4371b685f4030037f
franklin <igahfranklyn@gmail.com>
2024-11-30 21:57:25 +0000

    code deploy

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, World!"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
